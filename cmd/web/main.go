3b5443e11dc483dcf1e0a0c1d293db9a7c2ff72f
azekla <oazat331@gmail.com>
2024-10-20 23:07:27 +0500

    improvement for passing application configuration to handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/azekla/booking/pkg/config"
 	"github.com/azekla/booking/pkg/handlers"
+	"github.com/azekla/booking/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,12 +13,25 @@ const portNumber = ":7182"
 
 // the main function
 func main() {
+	var app config.AppConfig
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplate(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	err := http.ListenAndServe(portNumber, nil)
+	err = http.ListenAndServe(portNumber, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,15 +1,35 @@
 package handlers
 
 import (
+	"github.com/azekla/booking/pkg/config"
 	"github.com/azekla/booking/pkg/render"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl")
 }
