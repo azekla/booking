6e510517f4337e07600ce97a2cc8c594afa8c0ef
azekla <oazat331@gmail.com>
2024-10-25 17:57:22 +0500

    adding sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,24 +2,41 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/azekla/booking/pkg/config"
 	"github.com/azekla/booking/pkg/handlers"
 	"github.com/azekla/booking/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
+// set the port number for the server
 const portNumber = ":7182"
+
+var app config.AppConfig
+var session *scs.SessionManager
 
 // the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+ 	// create a new session manager
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	// assign the session manager to the app config
+	app.Session = session
 
+	// create a new template cache
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
 	}
 
+	// assign the template cache to the app config
 	app.TemplateCache = tc
 	app.UseCache = false
 	repo := handlers.NewRepo(&app)
